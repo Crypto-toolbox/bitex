3f42d5810f45039a523fcb329c23b6927c2d056c
nlsdfnbch <23okrs20+github@mykolab.com>
2016-06-13 21:30:15 +0200

    added some more basic funcs and extended existing ones

--- bitex/http/client.py
+++ bitex/http/client.py
@@ -5,7 +5,7 @@ Do fancy shit.
 
 # Import Built-Ins
 import logging
-
+from socket import AF_INET, SOCK_DGRAM, socket
 # Import Third-Party
 
 # Import Homebrew
@@ -16,17 +16,40 @@ log = logging.getLogger(__name__)
 
 class Client:
     """
-    Base Class for http clients.
+    Base Class for http clients. One client per pair!
     """
-    def __init__(self):
-        pass
+    def __init__(self, receiver_addr, exchange, name, pair):
+        """
+        Base Class for http clients
+        :param receiver_addr: tuple, ex ('localhost', 6666)
+        :param exchange: API Class
+        :param name: str, name of exchange
+        :param pair: str, pair as used in querying exchange.
+        """
+        self._receiver = receiver_addr
+        self._exchange = exchange
+        self._name = name
+        self._pair = pair
 
     def send(self, message):
-        pass
+        """
+        Send message to self._receiver
+        :param message: bytes
+        :return: None
+        """
+        sock = socket(AF_INET, SOCK_DGRAM)
+        sock.sendto(message, self._receiver)
 
     def run(self):
         pass
 
-    def format(self, string):
-        pass
+    def _format_ob(self, ls):
+        """
+        adds pair and exchange to list, converts to string and sends it out.
+        :param ls: list or field values
+        :return: str
+        """
+        ts_sent, ts_received, *rest = ls
+        new = [ts_sent, ts_received, self._pair, self._name, *rest]
+        return '\t'.join(new)
 
