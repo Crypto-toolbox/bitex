7e753bf632d822b558e0372e74e4feddc8c97f00
nlsdfnbch <23okrs20+github@mykolab.com>
2016-06-14 19:28:40 +0200

    added orderbook formatter func

--- bitex/http/kraken.py
+++ bitex/http/kraken.py
@@ -10,16 +10,30 @@ import time
 import json
 
 # Import Third-Party
-from ..api.kraken import API
+try:
+    from ..api.kraken import API
+except SystemError:
+    from bitex.api.kraken import API
 
 # Import Homebrew
-
+from bitex.http.client import Client
 log = logging.getLogger(__name__)
 
-class Client:
-    def __init__(self, server_addr, key=None):
-        self.__server_addr = server_addr
-        self.__key = key
+class KrakenHTTP(Client):
+    def __init__(self, server_addr, pair, key='', secret='', key_file=''):
+        api = API(key, secret)
+        if key_file:
+            api.load_key(key_file)
+        super(KrakenHTTP, self).__init__(server_addr, api, 'Kraken', pair)
+
+    def format_ob(self, sent, received, input):
+        ask_p, ask_v, ask_t = input['result'][self._pair]['asks'][0]
+        bid_p, bid_v, bid_t = input['result'][self._pair]['bids'][0]
+        formatted = [['ask', ask_t, ask_p, ask_v],
+                     ['bid', bid_t, bid_p, bid_v]]
+
+        for i in formatted:
+            self.send(super(KrakenHTTP, self)._format(sent, received, *i))
 
     def send(self, message):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -27,19 +41,19 @@ class Client:
         #                   self.__server_addr)
         print(message)
 
-    def listen(self, endpoint, q={}, private=False):
-        api = API()
+    def listen_ob(self, count=0):
+        q = {'pair': self._pair}
+        if count:
+            q['count'] = count
 
         while True:
             print("listening!")
-            if private:
-                api.load_key(self.__key)
-                resp = api.query_private(endpoint, q)
-            else:
-                resp = api.query_public(endpoint, q)
-            self.send(resp)
+            sent = time.time()
+            resp = self._listen('Depth', q)
+            received = time.time()
+            self.format_ob(sent, received, resp)
             time.sleep(5)
 
 if __name__ == '__main__':
-    uix = Client(('localhost', 6666), './kraken.key')
-    uix.listen('Balance', q={'asset':'ZEUR'}, private=True)
+    test = KrakenHTTP(('localhost', 67676), 'XXBTZEUR')
+    test.listen_ob()
