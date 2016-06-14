29e4bcdbf179ae3eae2eeef5666a3a2e34ca555c
nlsdfnbch <23okrs20+github@mykolab.com>
2016-06-14 19:30:28 +0200

    client base class now sends json data.

--- bitex/http/client.py
+++ bitex/http/client.py
@@ -5,6 +5,7 @@ Do fancy shit.
 
 # Import Built-Ins
 import logging
+import json
 from socket import AF_INET, SOCK_DGRAM, socket
 # Import Third-Party
 
@@ -18,16 +19,16 @@ class Client:
     """
     Base Class for http clients. One client per pair!
     """
-    def __init__(self, receiver_addr, exchange, name, pair):
+    def __init__(self, receiver_addr, api, name, pair):
         """
         Base Class for http clients
         :param receiver_addr: tuple, ex ('localhost', 6666)
-        :param exchange: API Class
+        :param api: API Class
         :param name: str, name of exchange
         :param pair: str, pair as used in querying exchange.
         """
         self._receiver = receiver_addr
-        self._exchange = exchange
+        self._api = api
         self._name = name
         self._pair = pair
 
@@ -40,16 +41,23 @@ class Client:
         sock = socket(AF_INET, SOCK_DGRAM)
         sock.sendto(message, self._receiver)
 
-    def run(self):
-        pass
+    def _listen(self, method, q, private=False):
 
-    def _format_ob(self, ls):
+        if private:
+            resp = self._api.query_private(method, q)
+        else:
+            resp = self._api.query_public(method, q)
+        return resp
+
+    def _format(self,sent, received, *ls):
         """
         adds pair and exchange to list, converts to string and sends it out.
+        :param sent: timestamp
+        :param received: timestamp
         :param ls: list or field values
         :return: str
         """
-        ts_sent, ts_received, *rest = ls
-        new = [ts_sent, ts_received, self._pair, self._name, *rest]
-        return '\t'.join(new)
+        new = [sent, received, self._pair, self._name, *ls]
+
+        return json.dumps(new)
 
