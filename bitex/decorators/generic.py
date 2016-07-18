ae8cfc88f1e333d54bf9822f585d7675c0c8e2d1
nlsdfnbch <23okrs35+github@mykolab.com>
2016-07-18 13:33:31 +0200

    added logging decorator, response checker decorator

--- bitex/decorators/generic.py
+++ bitex/decorators/generic.py
@@ -6,13 +6,14 @@ Do fancy shit.
 # Import Built-Ins
 import logging
 import time
+import json
+
 # Import Third-Party
 
 # Import Homebrew
 
 
 log = logging.getLogger(__name__)
-
 
 
 def time_resp(func):
@@ -26,4 +27,28 @@ def time_resp(func):
             resp = func(self, *args, **kwargs)
             received = time.time()
             return sent, received, resp
+    return wrapper
+
+
+def logging(func):
+    def wrapper(self, *args, **kwargs):
+        log.debug("%s: Args %s, Kwargs: %s", (func.__name__, args, kwargs))
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def validate_response(func):
+    def wrapper(self, *args, **kwargs):
+        resp = func(*args, **kwargs)
+
+        if resp.status_code != 200:
+            raise ConnectionError("Response Code was %s" % resp.status_code)
+
+        try:
+            resp.json()
+        except json.JSONDecodeError as e:
+            log.error("Returned data wasn't json-seriazable - possibly wrong url? Error: %s", e)
+            raise
+
+        return resp
     return wrapper
