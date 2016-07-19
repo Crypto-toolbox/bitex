cbb7d57c97e483ac92a574890b4222f44e7591b1
nlsdfnbch <23okrs35+github@mykolab.com>
2016-07-19 15:24:02 +0200

    added doc strings, args to overlay ref methods

--- bitex/http/overlay.py
+++ bitex/http/overlay.py
@@ -20,35 +20,83 @@ class HTTPClient:
     def __init__(self):
         pass
 
-    def ticker(self, pair):
+    def ticker(self, pair, **kwargs):
+        """
+        Returns Ticker of given pair
+        :param pair:
+        :param kwargs:
+        :return:
+        """
         pass
 
-    def order_book(self, pair):
+    def order_book(self, pair, **kwargs):
+        """
+        Returns order book for given pair.
+        :param pair:
+        :param kwargs:
+        :return:
+        """
         pass
 
-    def trades(self, pair):
+    def trades(self, pair, **kwargs):
+        """
+        Returns recent trades for given pair.
+        :param pair:
+        :param kwargs:
+        :return:
+        """
         pass
 
-    def balance(self, currency=None):
+    def balance(self, **kwargs):
+        """
+        Returns the balance of the exchange account's wallets.
+        :param kwargs:
+        :return:
+        """
         pass
 
-    def orders(self):
+    def orders(self, *args, **kwargs):
+        """
+        Return open orders.
+        :param pair:
+        :param kwargs:
+        :return:
+        """
         pass
 
     def ledger(self):
+        """
+        Ledger consists of recent trades, deposits, and withdrawals to and
+        from user's account.
+        :return:
+        """
         pass
 
-    def add_order(self, prive, vol, pair, ask_or_bid, order_type='limit'):
+    def add_order(self, prive, vol, pair, ask_or_bid, order_type='limit',
+                  **kwargs):
+        """
+        Places a bid or ask order of given order type.
+        :param prive:
+        :param vol:
+        :param pair:
+        :param ask_or_bid:
+        :param order_type:
+        :param kwargs:
+        :return:
+        """
         pass
 
     def cancel_order(self, uuid):
-        pass
-
-    def deposit_address(self):
-        pass
-
-    def withdraw(self, currency, vol, to_address):
+        """
+        cancels an order with the given uuid.
+        :param uuid:
+        :return:
+        """
         pass
 
     def fees(self):
+        """
+        Returns fees applicable at exchange.
+        :return:
+        """
         pass
