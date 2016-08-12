0a412277e8026fa8728ea30950586dc435c005d0
nlsdfnbch <23okrs35+github@mykolab.com>
2016-08-12 12:13:50 +0200

    updated version number and removed old sub modules

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from distutils.core import setup
 
-setup(name='BitEx', version='0.4', author='Nils Diefenbach',
+setup(name='BitEx', version='0.6', author='Nils Diefenbach',
       author_email='23okrs20+pypi@mykolab.com',
       url="https://github.com/nlsdfnbch/bitex.git",
-      packages=['bitex', 'bitex.api', 'bitex.decorators', 'bitex.http',
-                'bitex.postoffice'])
+      packages=['bitex', 'bitex.api', 'bitex.http'])
 
