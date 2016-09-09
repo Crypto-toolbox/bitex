6a1c31e3c3d82f5764201e7ea84bf80cf7439024
nlsdfnbch <23okrs35+github@mykolab.com>
2016-09-09 14:18:24 +0200

    removed http module; updated init accordingly.

--- bitex/http/__init__.py
+++
@@ -1,6 +0,0 @@
-from bitex.http.bitfinex import BitfinexHTTP
-from bitex.http.bitstamp import BitstampHTTP
-from bitex.http.coincheck import CoincheckHTTP
-from bitex.http.gdax import GdaxHTTP
-from bitex.http.kraken import KrakenHTTP
-from bitex.http.okcoin import OKCoinHTTP
