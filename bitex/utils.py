125534cbf1d3ab3f34d4961901221383dc51e815
nlsdfnbch <23okrs35+github@mykolab.com>
2016-09-29 14:01:47 +0200

    submodule for utility funcs added

---
+++ bitex/utils.py
@@ -0,0 +1,37 @@
+"""
+Task:
+Descripion of script here.
+"""
+
+# Import Built-Ins
+import logging
+import json
+# Import Third-Party
+
+# Import Homebrew
+
+# Init Logging Facilities
+log = logging.getLogger(__name__)
+
+
+def return_json(func):
+    """
+    Wrapper to add additional debugging information to response objects.
+
+    :param func:
+    :return:
+    """
+    def wrapper(*args, **kwargs):
+        try:
+            r = func(*args, **kwargs)
+        if r.status_code != 200:
+            log.error("return_json: Error while querying %s" % r.request.url)
+            raise ConnectionError("Returned Status Code was %s" % r.status_code)
+
+        try:
+            return r.json()
+        except json.JSONDecodeError:
+            log.error('return_json: Error while parsing json. '
+                      'Request url was: %s, result is: %s' % (r.request.url, r.text))
+            raise
+    return wrapper
