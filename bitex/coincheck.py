cf3211e4268ddebcac6a36f187f6ae4fd6219a7b
nlsdfnbch <23okrs35+github@mykolab.com>
2016-09-30 09:28:12 +0200

    added coincheck public func overlay class

---
+++ bitex/coincheck.py
@@ -0,0 +1,44 @@
+"""
+Task:
+Descripion of script here.
+"""
+
+# Import Built-Ins
+import logging
+
+# Import Third-Party
+
+# Import Homebrew
+from bitex.api.rest import CoincheckREST
+from bitex.utils import return_json
+
+# Init Logging Facilities
+#logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+
+class Coincheck(CoincheckREST):
+    def __init__(self, key='', secret='', key_file=''):
+        super(Coincheck, self).__init__(key, secret)
+        if key_file:
+            self.load_key(key_file)
+
+    def public_query(self, endpoint, **kwargs):
+        return self.query('GET', endpoint, **kwargs)
+
+    def private_query(self, endpoint, **kwargs):
+        return self.query('POST', endpoint, authenticate=True, **kwargs)
+
+    @return_json
+    def ticker(self, pair):
+        return self.public_query('ticker', params={'pair': pair})
+
+    @return_json
+    def trades(self, pair):
+        return self.public_query('trades', params={'pair': pair})
+
+    @return_json
+    def order_book(self, pair):
+        return self.public_query('order_book', params={'pair': pair})
+
+
