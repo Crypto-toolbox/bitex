a829fc9a1b5933f09ba032f760c8ce328ec5e185
nlsdfnbch <23okrs35+github@mykolab.com>
2016-09-30 12:47:51 +0200

    added wrapper for public endpoints at yunbi exchange

---
+++ bitex/yunbi.py
@@ -0,0 +1,70 @@
+"""
+https://yunbi.com/documents/api/guide
+"""
+
+# Import Built-Ins
+import logging
+
+# Import Third-Party
+
+# Import Homebrew
+from bitex.api.rest import YunbiREST
+from bitex.utils import return_json
+
+# Init Logging Facilities
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+
+class Yunbi(YunbiREST):
+    def __init__(self, key='', secret='', key_file=''):
+        super(Yunbi, self).__init__(key, secret)
+        if key_file:
+            self.load_key(key_file)
+        print(self.uri)
+
+    def public_query(self, endpoint, **kwargs):
+        return self.query('GET', endpoint + '.json', **kwargs)
+
+    def private_query(self, endpoint, **kwargs):
+        return self.query('POST', endpoint, authenticate=True, **kwargs)
+
+    @return_json
+    def pairs(self):
+        return self.public_query('symbols')
+
+    @return_json
+    def ticker(self, pair=None):
+        if pair:
+            return self.public_query('tickers/%s' % pair)
+        else:
+            return self.public_query('tickers')
+
+    @return_json
+    def ohlc(self, pair, **kwargs):
+        q = {'market': pair}
+        q.update(kwargs)
+        return self.public_query('k', params=q)
+
+    @return_json
+    def order_book(self, pair, **kwargs):
+        q = {'market': pair}
+        q.update(kwargs)
+        return self.public_query('order_book', params=q)
+
+    @return_json
+    def trades(self, pair, **kwargs):
+        q = {'market': pair}
+        q.update(kwargs)
+        return self.public_query('trades', params=q)
+
+    @return_json
+    def auction(self, pair):
+        return self.public_query('auction/%s' % pair)
+
+    @return_json
+    def auction_history(self, pair, **kwargs):
+        return self.public_query('auction/%s/history' % pair, params=kwargs)
+
+
+
