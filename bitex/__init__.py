30f53fbf280eea60e1b252aab0c716dacdf4d37d
nlsdfnbch <23okrs35+github@mykolab.com>
2016-11-02 09:03:50 +0100

    added poloniex to init files, included interfaces with setup.py

--- bitex/__init__.py
+++ bitex/__init__.py
@@ -1,4 +1,4 @@
 from bitex import api
 from bitex.interfaces import Kraken, Bitfinex, Bitstamp, CCEX, Coincheck
 from bitex.interfaces import Cryptopia, Gemini, ItBit, OKCoin, RockTradingLtd
-from bitex.interfaces import Yunbi, Bittrex
+from bitex.interfaces import Yunbi, Bittrex, Poloniex
