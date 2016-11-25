b671fdcac82378075c5fb9b559d98dba35fc2baf
nls <none@nothing.com>
2016-11-25 20:42:00 +0100

    added further support for bitex standardized methods in okcoin interface

--- bitex/interfaces/okcoin.py
+++ bitex/interfaces/okcoin.py
@@ -44,27 +44,37 @@ class OKCoin(OKCoinREST):
 
     @return_json(None)
     def bid(self, pair, price, amount, **kwargs):
-        raise NotImplementedError()
+        q = {'symbol': pair, 'price': price, 'amount': amount, 'type': 'buy'}
+        q.update(kwargs)
+        return self.private_query('trade.do', params=q)
 
     @return_json(None)
     def ask(self, pair, price, amount, **kwargs):
-        raise NotImplementedError()
+        q = {'symbol': pair, 'price': price, 'amount': amount, 'type': 'sell'}
+        q.update(kwargs)
+        return self.private_query('trade.do', params=q)
 
     @return_json(None)
-    def cancel_order(self, order_id, all=False, **kwargs):
-        raise NotImplementedError()
+    def cancel_order(self, order_id, **kwargs):
+        q = {'order_id': order_id}
+        q.update(kwargs)
+        return self.private_query('cancel_order.do', params=q)
 
     @return_json(None)
     def order(self, order_id, **kwargs):
-        raise NotImplementedError()
+        q = {'order_id': order_id}
+        q.update(kwargs)
+        return self.private_query('orders.info', params=q)
 
     @return_json(None)
     def balance(self, **kwargs):
-        raise NotImplementedError()
+        return self.private_query('userinfo.do', params=kwargs)
 
     @return_json(None)
-    def withdraw(self, _type, source_wallet, amount, tar_addr, **kwargs):
-        raise NotImplementedError()
+    def withdraw(self, amount, tar_addr, **kwargs):
+        q = {'withdraw_address': tar_addr, 'withdraw_amount': amount}
+        q.update(kwargs)
+        return self.private_query('withdraw.do', params=q)
 
     @return_json(None)
     def deposit_address(self, **kwargs):
