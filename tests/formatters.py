6eca83c1a2b85a44147ec4caf2e289b106088546
nlsdfnbch <23okrs35+github@mykolab.com>
2017-01-11 13:21:26 +0100

    added emptry formatter test file

---
+++ tests/formatters.py
@@ -0,0 +1,13 @@
+# Import Built-Ins
+import logging
+from unittest import TestCase
+# Import Third-Party
+
+# Import Homebrew
+
+# Init Logging Facilities
+log = logging.getLogger(__name__)
+
+
+class FormatterTest(TestCase):
+     pass
