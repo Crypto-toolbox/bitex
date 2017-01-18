52a96e6b44dc235570472810ae4078cbd30eb880
nls <nls@debian>
2017-01-18 11:03:43 +0000

    Implemented Code Review #34

--- bitex/utils.py
+++ bitex/utils.py
@@ -1,7 +1,13 @@
+"""
+Provides utility functions, used across more than one module or sub module.
+
+"""
+
 # Import Built-Ins
 import logging
 import json
 import requests
+from functools import wraps
 # Import Third-Party
 
 # Import Homebrew
@@ -11,32 +17,42 @@ log = logging.getLogger(__name__)
 
 
 def return_json(formatter=None):
+    """
+    Decorator, which Applies the referenced formatter to the function output
+    (expects requests.response object). If `formatter` is `None`, returns the
+    json of the response.
+    :param formatter: bitex.formatters.Formatter() obj
+    :return: json_data, raw if formatter is None, else formatted_json, raw
+    :return type: requests.response.json(), requests.response() obj ||
+                  formatted_json, requests.response() obj
+    """
     def decorator(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 r = func(*args, **kwargs)
-            except Exception as e:
-                log.error("return_json(): Error during call to "
-                          "%s(%s, %s) %s" % (func.__name__, args, kwargs, e))
+            except Exception:
+                log.exception("return_json(): Error during call to %s(%s, %s)",
+                              func.__name__, args, kwargs)
                 raise
 
             try:
                 r.raise_for_status()
-            except requests.HTTPError as e:
-                log.error("return_json: HTTPError for url %s: "
-                          "%s" % (r.request.url, e))
+            except requests.HTTPError:
+                log.exception("return_json: HTTPError for url %s",
+                              r.request.url)
                 return None, r
 
             try:
                 data = r.json()
             except json.JSONDecodeError:
-                log.error('return_json: Error while parsing json. '
-                          'Request url was: %s, result is: '
-                          '%s' % (r.request.url, r.text))
+                log.exception('return_json: Error while parsing json. '
+                              'Request url was: %s, result is: '
+                              '%s', r.request.url, r.text)
                 return None, r
-            except Exception as e:
-                log.error("return_json(): Unexpected error while parsing json "
-                          "from %s: %s" % (r.request.url, e))
+            except Exception:
+                log.exception("return_json(): Unexpected error while parsing "
+                              "json from %s", r.request.url)
                 raise
 
             # Apply formatter and return
