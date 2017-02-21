2e401fcd16d1275ceed2f05f61f6b7e5ee1f958b
nlsdfnbch <23okrs35+github@mykolab.com>
2017-02-21 11:15:38 +0100

    updated version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='BitEx', version='1.0.2', author='Nils Diefenbach',
+setup(name='BitEx', version='1.0.3', author='Nils Diefenbach',
       author_email='23okrs20+pypi@mykolab.com',
       url="https://github.com/nlsdfnbch/bitex.git",
       packages=['bitex', 'bitex.api', 'bitex.interfaces', 'bitex.formatters'])
