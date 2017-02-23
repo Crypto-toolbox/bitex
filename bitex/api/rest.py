a121b740e81b3632f6e5da9ea84168aaa4111484
nlsdfnbch <23okrs35+github@mykolab.com>
2017-02-23 13:05:37 +0100

    #30 - added Vaultoro REST API client

--- bitex/api/rest.py
+++ bitex/api/rest.py
@@ -551,3 +551,27 @@ class HitBTCREST(APIClient):
                              msg.encode(encoding='utf-8'), hashlib.sha512)
         headers = {'Api-signature': signature}
         return self.uri + msg, {'headers': headers, 'data': params}
+
+
+class VaultoroREST(APIClient):
+    def __init__(self, key=None, secret=None, api_version=None,
+                 url='https://api.vaultoro.com/', timeout=5):
+        api_version = '' if not api_version else api_version
+        super(VaultoroREST, self).__init__(url, api_version=api_version,
+                                           key=key, secret=secret,
+                                           timeout=timeout)
+
+    def sign(self, uri, endpoint, endpoint_path, method_verb, *args, **kwargs):
+        try:
+            params = kwargs['params']
+        except KeyError:
+            params = {}
+        nonce = self.nonce()
+        kwargs['nonce'] = nonce
+        kwargs['apikey'] = self.key
+        msg = uri + urllib.parse.urlencode(params)
+
+        signature = hmac.new(self.secret.encode(encoding='utf-8'),
+                             msg.encode(encoding='utf-8'), hashlib.sha256).hexdigest()
+        headers = {'X-Signature': signature}
+        return msg, {'headers': headers}
