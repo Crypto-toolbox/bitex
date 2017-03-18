04262644ffed7087016ffb825605e758ff349c94
nlsdfnbch <23okrs20+github@mykolab.com>
2017-03-18 07:24:37 +0100

    Added GDAX to global import

--- bitex/__init__.py
+++ bitex/__init__.py
@@ -2,3 +2,4 @@ from bitex import api
 from bitex.interfaces import Kraken, Bitfinex, Bitstamp, CCEX, Coincheck
 from bitex.interfaces import Cryptopia, Gemini, ItBit, OKCoin, RockTradingLtd
 from bitex.interfaces import Yunbi, Bittrex, Poloniex, Quoine, QuadrigaCX
+from bitex.interfaces import GDAX
