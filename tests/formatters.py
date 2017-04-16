3d797b9ffb04696535c1e4665f5fdac58dfafa7b
nlsdfnbch <23okrs20+github@mykolab.com>
2017-04-16 14:26:42 +0200

    #58 added DASH pairs to kraken test case

--- tests/formatters.py
+++ tests/formatters.py
@@ -17,8 +17,10 @@ log = logging.getLogger(__name__)
 class FormatterTest(TestCase):
     def test_krknFormatter_format_pair_works_correctly(self):
         fmt = KrknFormatter()
-        test_pairs = ['btcusd', 'ltcbtc', 'xmr_btc', 'xxbtxltc']
-        expected_output = ['XXBTZUSD', 'XLTCXXBT', 'XXMRXXBT', 'XXBTXLTC']
+        test_pairs = ['btcusd', 'ltcbtc', 'xmr_btc', 'xxbtxltc', 'DaShBTC',
+                      'dasheur']
+        expected_output = ['XXBTZUSD', 'XLTCXXBT', 'XXMRXXBT', 'XXBTXLTC',
+                           'DASHXBT', 'DASHZEUR']
         fmt_output = [fmt.format_pair(pair) for pair in test_pairs]
         self.assertEqual(fmt_output, expected_output)
 
