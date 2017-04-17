24182be230727e04586ac321b54f08e7080f6cb8
nlsdfnbch <23okrs20+github@mykolab.com>
2017-04-17 19:50:18 +0200

    renamed formatter tests, moved them to separate testcases

--- tests/formatters.py
+++ tests/formatters.py
@@ -13,9 +13,12 @@ from bitex.formatters.bittrex import BtrxFormatter
 # Init Logging Facilities
 log = logging.getLogger(__name__)
 
+# Sample data for tests can be found at
+# './test_data/%s_%s.json' % (exchange_name, func_name)
 
-class FormatterTest(TestCase):
-    def test_krknFormatter_format_pair_works_correctly(self):
+
+class KrakenFormatterTest(TestCase):
+    def test_format_pair_works_correctly(self):
         fmt = KrknFormatter()
         test_pairs = ['btcusd', 'ltcbtc', 'xmr_btc', 'xxbtxltc', 'DaShBTC',
                       'dasheur']
@@ -24,21 +27,27 @@ class FormatterTest(TestCase):
         fmt_output = [fmt.format_pair(pair) for pair in test_pairs]
         self.assertEqual(fmt_output, expected_output)
 
-    def test_BtstFormatter_format_pair_works_correctly(self):
+
+class BitstampFormatterTest(TestCase):
+    def test_format_pair_works_correctly(self):
         fmt = BtstFormatter()
         test_pairs = ['btcusd', 'ltcbtc', 'xmr_btc', 'BTCEUR']
         expected_output = ['btcusd', 'ltcbtc', 'xmrbtc', 'btceur']
         fmt_output = [fmt.format_pair(pair) for pair in test_pairs]
         self.assertEqual(fmt_output, expected_output)
 
-    def test_BtfxFormatter_format_pair_works_correctly(self):
+
+class BitfinexFormatterTest(TestCase):
+    def test_format_pair_works_correctly(self):
         fmt = BtfxFormatter()
         test_pairs = ['btcusd', 'ltcbtc', 'xmr_btc', 'BTCEUR']
         expected_output = ['BTCUSD', 'LTCBTC', 'XMRBTC', 'BTCEUR']
         fmt_output = [fmt.format_pair(pair) for pair in test_pairs]
         self.assertEqual(fmt_output, expected_output)
 
-    def test_BtrxFormatter_format_pair_works_correctly(self):
+
+class BittrexFormatterTest(TestCase):
+    def test_format_pair_works_correctly(self):
         fmt = BtrxFormatter()
         test_pairs = ['btcusd', 'ltcbtc', 'xmr_btc', 'BTCEUR']
         expected_output = ['BTC-USD', 'BTC-LTC', 'XMR-BTC', 'BTC-EUR']
