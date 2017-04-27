fc0bc6b76987b437eed9a1c2fbd25d3d157b6197
nlsdfnbch <23okrs20+github@mykolab.com>
2017-04-27 20:29:13 +0200

    #55 Passed format_pair test

--- bitex/formatters/gemini.py
+++ bitex/formatters/gemini.py
@@ -12,6 +12,10 @@ log = logging.getLogger(__name__)
 
 class GmniFormatter(Formatter):
 
+    def format_pair(self, input_pair):
+        base, quote = super(GmniFormatter, self).format_pair(input_pair)
+        return base.lower() + quote.lower()
+
     @staticmethod
     def ticker(data, *args, **kwargs):
         return (data['bid'], data['ask'], None, None, None, None, data['last'],
