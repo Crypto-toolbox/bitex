419f5a8978f6a33721cb998d55fc689fd06a7485
nlsdfnbch <23okrs20+github@mykolab.com>
2017-04-27 20:34:06 +0200

    #55 Passed format_pair test

--- bitex/formatters/quadriga.py
+++ bitex/formatters/quadriga.py
@@ -12,6 +12,10 @@ log = logging.getLogger(__name__)
 
 class QuadrigaCXFormatter(Formatter):
 
+    def format_pair(self, input_pair):
+        base, quote = super(QuadrigaCXFormatter, self).format_pair(input_pair)
+        return base.lower() + '_' + quote.lower()
+
     @staticmethod
     def ticker(data, *args, **kwargs):
         return (data['bid'], data['ask'], data['high'], data['low'],
