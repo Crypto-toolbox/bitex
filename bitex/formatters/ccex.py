bc369d3137f59e7d05ea39d96c3719b439261a45
nlsdfnbch <23okrs20+github@mykolab.com>
2017-04-27 20:07:35 +0200

    Revert "#55 Passed format_pair test"
    This reverts commit be4ed6ed66a4471112ec847aac25c5a5951883c3.

--- bitex/formatters/ccex.py
+++ bitex/formatters/ccex.py
@@ -12,10 +12,6 @@ log = logging.getLogger(__name__)
 
 class CcexFormatter(Formatter):
 
-    def format_pair(self, input_pair):
-        base, quote = super(CcexFormatter, self).format_pair(input_pair)
-        return base.lower() + '-' + quote.lower()
-
     @staticmethod
     def ticker(data, *args, **kwargs):
         return (data['buy'], data['sell'], data['high'], data['low'], None,
