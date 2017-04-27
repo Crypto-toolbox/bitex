f7804777885a11f4c237cd31f013fe839bcb2528
nlsdfnbch <23okrs20+github@mykolab.com>
2017-04-27 19:41:31 +0200

    passed formatter test for bitstamp #55

--- bitex/formatters/bitstamp.py
+++ bitex/formatters/bitstamp.py
@@ -12,6 +12,10 @@ log = logging.getLogger(__name__)
 
 class BtstFormatter(Formatter):
 
+    def format_pair(self, input_pair):
+        base, quote = super(BtstFormatter, self).format_pair(input_pair)
+        return base.lower() + quote.lower()
+
     @staticmethod
     def ticker(data, *args, **kwargs):
         return (data['bid'], data['ask'], data['high'], data['low'], data['open'],
