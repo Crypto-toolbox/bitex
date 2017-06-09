5c02b435777bd48f76e1ba8d7014c347dfbae4d7
nlsdfnbch <23okrs35+github@mykolab.com>
2017-06-09 16:59:17 +0200

    updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(name='BitEx', version='2.0.0', author='Nils Diefenbach',
       author_email='23okrs20+pypi@mykolab.com',
       url="https://github.com/nlsdfnbch/bitex.git",
-      packages=['bitex', 'bitex.api', 'bitex.interfaces'],
+      packages=['bitex'],
       install_requires=['requests', 'websocket-client', 'autobahn',
                         'pusherclient'],
       description='Python3-based API Framework for Crypto Exchanges',
