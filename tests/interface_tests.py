49ab089274097341870945d1fb067e5be0afb390
nlsdfnbch <23okrs35+github@mykolab.com>
2017-06-20 09:29:35 +0200

    added basic Interface test case

--- tests/interface_tests.py
+++ tests/interface_tests.py
@@ -5,14 +5,17 @@ import unittest
 # Import Third-Party
 
 # Import Homebrew
+from bitex.interface import Interface
+from bitex.exceptions import UnsupportedPairError
 
 # Init Logging Facilities
 log = logging.getLogger(__name__)
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class InterfaceTests(unittest.TestCase):
+    def test_init_raises_NotImplementedError_for_basic_interface(self):
+        with self.assertRaises(NotImplementedError):
+            Interface(name='TestInterface', rest_api=None)
 
 
 if __name__ == '__main__':
