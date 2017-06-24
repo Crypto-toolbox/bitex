0959e7fd617b6bedd91b342fa50a1685d732380f
nlsdfnbch <23okrs20+github@mykolab.com>
2017-06-24 10:40:57 +0200

    added missing import, removed old check method

--- bitex/utils.py
+++ bitex/utils.py
@@ -1,23 +1,5 @@
 from functools import wraps
-
-@wraps
-def check_bitfinex_version_compatibility(func, *args, **kwargs):
-    v2_endpoints = ['wallets', 'orders', 'order_trades', 'positions',
-                    'offers', 'funding_info', 'performance', 'alert_list',
-                    'alert_set', 'alert_delete', 'calc_available_balance',
-                    'market_average_price', 'candles', 'ticker', 'tickers']
-    def wrapper(*args, **kwargs):
-        if func.__name__ in v2_endpoints and args[0].api.version != 'v2':
-            raise UnsupportedEndpointError('This endpoint is only available for'
-                                           'api version v2 (current is %s)'
-                                           % self.api.version)
-        elif func.__name__ not in v2_endpoints and args[0].api.version == 'v2':
-            raise UnsupportedEndpointError('This endpoint is only available for'
-                                           'api version v1 (current is %s)'
-                                           % self.api.version)
-        else:
-            return func(*args, **kwargs)
-    return wrapper
+from .exceptions import UnsupportedEndpointError
 
 #@wraps
 def check_compatibility(**version_func_pairs):
@@ -34,16 +16,19 @@ def check_compatibility(**version_func_pairs):
         d = {value: k for value in version_func_pairs[k]}
         method_compatibility.update(d)
 
+
     def decorator(func):
         def wrapped(*args, **kwargs):
             interface = args[0]
-            if (method_compatibility[func.__name__].replace('_', '.') !=
-                interface.REST.version):
-                raise UnsupportedEndpointError("Method not available on this API"
-                                               "version (current is %s, "
-                                               "supported is %s)" %
-                                               (interface.REST.version,
-                                                method_compatibility[func.__name__]))
+            for version, methods in version_func_pairs.items():
+                if func.__name__ in methods:
+                    if version.replace('_', '.') != interface.REST.version:
+                        error_msg = ("Method not available on this API version"
+                                    "(current is %s, supported is %s)" %
+                                    (interface.REST.version,
+                                     version.replace('_', '.')))
+                        raise UnsupportedEndpointError(error_msg)
+
             return func(*args, **kwargs)
         return wrapped
     return decorator
