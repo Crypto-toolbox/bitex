45ceabe6e2baeb4e44465dd187773683f49116b1
nlsdfnbch <23okrs35+github@mykolab.com>
2017-08-01 10:37:28 +0200

    added untested coincheck implementation

--- bitex/interface.py
+++ bitex/interface.py
@@ -1010,38 +1010,77 @@ class CCEX(RESTInterface):
 
 
 class CoinCheck(RESTInterface):
+    """Interface Class for the Coincheck.com REST API.
+
+    Documentation:
+        https://coincheck.com/documents/exchange/api
+
+    The API documentation appears to be not up-to-date, or the endpoints
+    not updated to support the various new pairs at the exchange.
+
+    """
     def __init__(self, **APIKwargs):
         super(CoinCheck, self).__init__('CoinCheck',
                                         CoincheckREST(**APIKwargs))
 
+    def _get_supported_pairs(self):
+        return ['btc-jpy']
+
     # Public Endpoints
     def ticker(self, pair, *args, **kwargs):
-        raise NotImplementedError
+        return self.request('GET', 'ticker', params=kwargs)
 
     def order_book(self, pair, *args, **kwargs):
-        raise NotImplementedError
+        return self.request('GET', 'order_books', params=kwargs)
 
     def trades(self, pair, *args, **kwargs):
-        raise NotImplementedError
+        return self.request('GET', 'trades', params=kwargs)
 
     # Private Endpoints
+    def _place_order(self, pair, price, size, side, **kwargs):
+        payload = {'rate': price, 'amount': size, 'pair': pair,
+                   'order_type': side}
+        payload.update(kwargs)
+        return self.request('POST', 'exchange/orders', params=payload,
+                            authenticate=True)
+
     def ask(self, pair, price, size, *args, **kwargs):
-        raise NotImplementedError
+        if 'order_type' in kwargs:
+            if (kwargs['order_type'] not in
+                    ('sell', 'market_sell', 'leverage_sell', 'close_short')):
+                raise ValueError("order_type not supported by this function!")
+        return self._place_order(pair, price, size, 'sell', **kwargs)
 
     def bid(self, pair, price, size, *args, **kwargs):
-        raise NotImplementedError
+        if 'order_type' in kwargs:
+            if (kwargs['order_type'] not in
+                    ('buy', 'market_buy', 'leverage_buy', 'close_long')):
+                raise ValueError("order_type not supported by this function!")
+        return self._place_order(pair, price, size, 'sell', **kwargs)
 
     def order_status(self, order_id, *args, **kwargs):
         raise NotImplementedError
 
     def open_orders(self, *args, **kwargs):
-        raise NotImplementedError
+        return self.request('GET', 'exchange/orders/open', params=kwargs,
+                            authenticate=True)
 
     def cancel_order(self, *order_ids, **kwargs):
-        raise NotImplementedError
+        if isinstance(order_ids, tuple):
+            result = []
+            for oid in order_ids:
+                payload = {'order_id': oid}
+                r = self.request('DELETE', 'exchange/orders/' + oid,
+                                 params=kwargs, authenticate=True)
+                result.append(r)
+            return r if len(r) > 1 else r[0]
+        else:
+            return self.request('DELETE', 'exchange/orders/' + order_ids,
+                                params=kwargs, authenticate=True)
 
     def wallet(self, *args, **kwargs):
-        raise NotImplementedError
+        return self.request('GET', 'accounts/balance', params=kwargs,
+                            authenticate=True)
 
 
 class Cryptopia(RESTInterface):
