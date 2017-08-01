9e05684be7ed8f01c58bea40a09cb579cb60b236
nlsdfnbch <23okrs35+github@mykolab.com>
2017-08-01 14:38:58 +0200

    added HitBtc Interface method code

--- bitex/interface.py
+++ bitex/interface.py
@@ -1145,6 +1145,76 @@ class HitBTC(RESTInterface):
     def __init__(self, **APIKwargs):
         super(HitBTC, self).__init__('HitBTC', HitBTCREST(**APIKwargs))
 
+    def _get_supported_pairs(self):
+        r = self.request('symbols').json()
+        return [entry['symbol'] for entry in r['symbols']]
+
+    def request(self, endpoint, authenticate=False, verb=None, **req_kwargs):
+        verb = verb if verb else 'GET'
+        if authenticate:
+            endpoint = 'trading/' + endpoint
+        else:
+            endpoint = 'public/' + endpoint
+        return super(HitBTC, self).request(verb, endpoint, authenticate,
+                                           **req_kwargs)
+
+    # Public Endpoints
+    @check_and_format_pair
+    def ticker(self, pair, *args, **kwargs):
+        return self.request('%s/ticker' % pair, params=kwargs)
+
+    @check_and_format_pair
+    def order_book(self, pair, *args, **kwargs):
+        return self.request('%s/orderbook' % pair, params=kwargs)
+
+    @check_and_format_pair
+    def trades(self, pair, *args, **kwargs):
+        if 'from' not in kwargs:
+            return self.request('%s/trades/recent', params=kwargs)
+        else:
+            return self.request('%s/trades', params=kwargs)
+
+    # Private Endpoints
+    def _place_order(self, pair, price, size, side, *args, **kwargs):
+        payload = {'symbol': pair, 'side': side, 'price': price,
+                   'quantity': size, 'type': 'limit'}
+        payload.update(kwargs)
+        return self.request('new_order', authenticate=True, verb='POST',
+                            params=kwargs)
+
+    @check_and_format_pair
+    def ask(self, pair, price, size, *args, **kwargs):
+        return self._place_order(pair, price, size, 'sell')
+
+    @check_and_format_pair
+    def bid(self, pair, price, size, *args, **kwargs):
+        return self._place_order(pair, price, size, 'buy')
+
+    def order_status(self, order_id, *args, **kwargs):
+        payload = {'client_order_id': order_id}
+        payload.update(kwargs)
+        return self.request('order', params=payload, authenticate=True)
+
+    def open_orders(self, *args, **kwargs):
+        return self.request('orders/active', authenticate=True, params=kwargs)
+
+    def cancel_order(self, *order_ids, cancel_all=False, **kwargs):
+        if cancel_all:
+            return self.request('cancel_orders', authenticate=True, verb='POST',
+                                params=kwargs)
+        else:
+            results = []
+            payload = kwargs
+            for oid in order_ids:
+                payload.update({'clientOrderId': oid})
+                r = self.request('cancel_order', authenticate=True,
+                                 verb='POST', params=payload)
+                results.append(r)
+            return results if len(results) > 1 else results[0]
+
+    def wallet(self, *args, **kwargs):
+        return self.request('balance', authenticate=True, params=kwargs)
+
 
 class Kraken(RESTInterface):
     def __init__(self, **APIKwargs):
