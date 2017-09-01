1bcc515f0e1b095fcb9a4693d10a38b7e971ff23
nlsdfnbch <23okrs35+github@mykolab.com>
2017-09-01 13:33:40 +0200

    Added Vaultoro methods

--- bitex/interface/extensions.py
+++ bitex/interface/extensions.py
@@ -1510,3 +1510,60 @@ class TheRockTrading(RESTInterface):
 class Vaultoro(RESTInterface):
     def __init__(self, **APIKwargs):
         super(Vaultoro, self).__init__('Vaultoro', VaultoroREST(**APIKwargs))
+
+    def request(self, endpoint, authenticate=False, post=False, **req_kwargs):
+        verb = 'GET' if not post else 'POST'
+        return super(Vaultoro, self).request(verb, endpoint, authenticate, **req_kwargs)
+
+    def _get_supported_pairs(self):
+        return ['BTC-GLD']
+
+    # Public Endpoints
+    @check_and_format_pair
+    def ticker(self, pair, *args, **kwargs):
+        return self.request('https://api.vaultoro.com/markets', params=kwargs)
+
+    @check_and_format_pair
+    def order_book(self, pair, *args, **kwargs):
+        return self.request('https://api.vaultoro.com/orderbook/', params=kwargs)
+
+    @check_and_format_pair
+    def trades(self, pair, *args, **kwargs):
+        return self.request('https://api.vaultoro.com/latesttrades', params=kwargs)
+
+    # Private Endpoints
+    def _place_order(self, pair, price, size, side, market_order, **kwargs):
+        order_type = 'limit' if not market_order else 'market'
+        q = {'gld': size, 'price': price}
+        return self.request('https://api.vaultoro.com/1/%s/gld/%s' %
+                            (side, order_type), authenticate=True,
+                            post=True, params=q)
+
+    @check_and_format_pair
+    def ask(self, pair, price, size, *args, market_order=False, **kwargs):
+        return self._place_order(pair, price, size, 'sell', market_order,
+                                 **kwargs)
+
+    @check_and_format_pair
+    def bid(self, pair, price, size, *args, market_order=False, **kwargs):
+        return self._place_order(pair, price, size, 'buy', market_order,
+                                 **kwargs)
+
+    def order_status(self, order_id, *args, **kwargs):
+        raise NotImplementedError
+
+    def open_orders(self, *args, **kwargs):
+        return self.request('https://api.vaultoro.com/1/orders',
+                            authenticate=True, params=kwargs)
+
+    def cancel_order(self, *order_ids, **kwargs):
+        results = []
+        for oid in order_ids:
+            r = self.request('https://api.vaultoro.com/1/cancel/%s' % oid,
+                             authenticate=True, post=True, params=kwargs)
+            results.append(r)
+        return results if len(results) > 1 else results[0]
+
+    def wallet(self, *args, **kwargs):
+        return self.request('https://api.vaultoro.com/1/balance',
+                            authenticate=True, params=kwargs)
