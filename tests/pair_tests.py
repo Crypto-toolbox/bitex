822a9b12313d60d65a83076942e3d2fa14f5aa1c
Muktar Attah <mukz@Muktars-MacBook-Pro.local>
2017-09-09 17:53:30 +0100

    Binance Pair Formatter and test

--- tests/pair_tests.py
+++ tests/pair_tests.py
@@ -18,6 +18,7 @@ class PairTests(unittest.TestCase):
         self.assertEqual(pair.format_for('Kraken'), 'XXBTZUSD')
         self.assertEqual(pair.format_for('Bitstamp'), 'btcusd')
         self.assertEqual(pair.format_for('Bitfinex'), 'BTCUSD')
+        self.assertEqual(pair.format_for('Binance'), 'BTCUSD')
         self.assertEqual(pair.format_for('Bittrex'), 'USD-BTC')
         self.assertEqual(pair.format_for('CoinCheck'), 'btc_usd')
         self.assertEqual(pair.format_for('GDAX'), 'BTC-USD')
@@ -35,6 +36,7 @@ class PairTests(unittest.TestCase):
         self.assertEqual(pair.format_for('HitBTC'), 'BTCUSD')
         self.assertEqual(pair.format_for('Vaultoro'), 'BTC-USD')
         self.assertEqual(pair.format_for('Bter'), 'btc_usd')
+
 
         # Assert that calling the formatter returns the standard presentation
         self.assertEqual(str(pair), 'BTCUSD')
