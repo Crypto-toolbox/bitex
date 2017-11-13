53875086b7904d0fe6b71762a6d5b07afe82e627
nlsdfnbch <23okrs20+github@mykolab.com>
2017-11-13 11:52:17 +0100

    updated name of logger

--- bitex/__init__.py
+++ bitex/__init__.py
@@ -1,5 +1,5 @@
 import logging
-logging.getLogger(__main__).warning("The API clients available in this package are be depreceated "
+logging.getLogger(__name__).warning("The API clients available in this package are be depreceated "
                                     "and will be no longer available in their current form "
                                     "starting with version 2.0!")
 from bitex.interfaces import Kraken, Bitfinex, Bitstamp, CCEX, Coincheck
