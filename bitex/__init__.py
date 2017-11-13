c69654933e38f15fedc1c215aea41da2d0ea984b
nlsdfnbch <23okrs20+github@mykolab.com>
2017-11-13 11:51:16 +0100

    added deprecation notice #100

--- bitex/__init__.py
+++ bitex/__init__.py
@@ -1,4 +1,9 @@
+import logging
+logging.getLogger(__main__).warning("The API clients available in this package are be depreceated "
+                                    "and will be no longer available in their current form "
+                                    "starting with version 2.0!")
 from bitex.interfaces import Kraken, Bitfinex, Bitstamp, CCEX, Coincheck
 from bitex.interfaces import Cryptopia, Gemini, ItBit, OKCoin, RockTradingLtd
 from bitex.interfaces import Yunbi, Bittrex, Poloniex, Quoine, QuadrigaCX
 from bitex.interfaces import Vaultoro, HitBtc, Bter, GDAX
+
