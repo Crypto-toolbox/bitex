140a2fef1a6887cf6bf63da28385a3beab22dc61
Nils Diefenbach <23okrs20+github@mykolab.com>
2017-11-14 09:39:57 +0100

    increase factor for good measure

--- bitex/api/REST/api.py
+++ bitex/api/REST/api.py
@@ -53,7 +53,7 @@ class APIClient(metaclass=ABCMeta):
         Creates a Nonce value for signature generation
         :return:
         """
-        return str(round(1000 * time.time())) 
+        return str(round(100000 * time.time())) 
 
     @staticmethod
     def api_request(*args, **kwargs):
