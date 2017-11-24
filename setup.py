2fe573e3379e6f08f5cf9d75515cfc5b9ba19863
nlsdfnbch <23okrs35+github@mykolab.com>
2017-11-24 10:41:19 +0100

    added nose as test runner

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='BitEx', version='2.0.0b4', author='Nils Diefenbach',
       author_email='23okrs20+pypi@mykolab.com',
       url="https://github.com/nlsdfnbch/bitex.git",
-      packages=['bitex', 'bitex.api', 'bitex.api.REST', 'bitex.interface'],
+      test_suite='nose.collector', tests_require=['nose'],
+      packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'travis']),
       install_requires=['requests', 'pyjwt'],
       description='Python3-based API Framework for Crypto Exchanges',
       license='MIT',  classifiers=['Development Status :: 4 - Beta',
