f4c2db68a878f32efae799ef09c2f8e2a727c06e
nlsdfnbch <23okrs35+github@mykolab.com>
2017-11-24 15:27:46 +0100

    use regex to fiter rc tags properly

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,4 +1,4 @@
-if [[$TRAVIS_TAG == *rc ]]
+if [[$TRAVIS_TAG == ^.*rc[0-9][0-9]*$ ]]
     python generate_rc_setup.py $TRAVIS_TAG
 fi
 python setup.py sdist && twine upload --username $PYPI_NAME --password $PYPI_PW dist/*
