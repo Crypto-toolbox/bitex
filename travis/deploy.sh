dcc8b36cb33c3246ad4395f727a8b8cdc1d7184e
nlsdfnbch <23okrs35+github@mykolab.com>
2017-11-24 13:23:51 +0100

    added custom deploy

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 python setup.py sdist;
-for file in dist/*.tar.gz; do mv $file ${file//.tar.gz/$TAG.tar.gz} ; done;
-twine upload --username $PYPI_NAME --password $PYPI_PW dist/*;
+if [[ $BRANCH != "master" ]]
+    for file in dist/*.tar.gz; do mv $file ${file//.tar.gz/$TAG.tar.gz} ; done
+fi
+twine upload --username $PYPI_NAME --password $PYPI_PW dist/*
