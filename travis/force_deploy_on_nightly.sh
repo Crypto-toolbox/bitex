09c9a24c24934f457b0b3e9946018485066591f9
nlsdfnbch <23okrs35+github@mykolab.com>
2017-11-24 13:56:28 +0100

    Require Travis Branch nightly to deploy

--- travis/force_deploy_on_nightly.sh
+++ travis/force_deploy_on_nightly.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-python setup.py sdist;
-if [[ $BRANCH != "master" ]]
-    for file in dist/*.tar.gz; do mv $file ${file//.tar.gz/$TAG.tar.gz} ; done
+if [[$TRAVIS_BRANCH == "nightly" ]]
+    python setup.py sdist
+    twine upload --username $PYPI_NAME --password $PYPI_PW dist/*
 fi
-twine upload --username $PYPI_NAME --password $PYPI_PW dist/*
