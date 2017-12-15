52f0aceb71e82b06101ef850fe60d10a79b73d2e
Hugh Macdonald <hugh@hughmacdonald.co.uk>
2017-12-15 20:41:25 +0000

    BinanceREST : Add implementation of BinanceREST class

--- bitex/__init__.py
+++ bitex/__init__.py
@@ -5,7 +5,7 @@ Email: 23okrs20+github@mykolab.com
 Repository at: https://github.com/nlsdfnbch/bitex
 License: MIT
 """
-from bitex.api.REST import BitstampREST, BterREST, BitfinexREST
+from bitex.api.REST import BinanceREST, BitstampREST, BterREST, BitfinexREST
 from bitex.api.REST import CryptopiaREST, CoincheckREST, CCEXREST
 from bitex.api.REST import GDAXREST, GeminiREST
 from bitex.api.REST import HitBTCREST
