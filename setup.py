d5a1ed2fdbebb84d69f807ec3839d2ad862e8269
nlsdfnbch <23okrs35+github@mykolab.com>
2018-01-09 10:50:43 +0100

    fix version num

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 
-setup(name='BitEx', version='1.2.12python', author='Nils Diefenbach',
+setup(name='BitEx', version='1.2.12', author='Nils Diefenbach',
       author_email='23okrs20+pypi@mykolab.com',
       url="https://github.com/nlsdfnbch/bitex.git",
       packages=['bitex', 'bitex.api', 'bitex.api.WSS', 'bitex.api.REST',
