c3b22bd7f95aaf6a3ae6323003a59d1c71a51b34
nlsdfnbch <23okrs35+github@mykolab.com>
2018-01-18 12:58:07 +0100

    update base test

--- tests/helpers.py
+++ tests/helpers.py
@@ -22,117 +22,115 @@ class MockResponse(requests.Response):
         return self.json_data
 
 
-class StandardizedMethodTests(TestCase):
-    def __init__(self, exchange, *args, **kwargs):
-        self.exchange = exchange
-        super(StandardizedMethodTests, self).__init__(*args, **kwargs)
+class BaseInterfaceTests:
+    class StandardizedMethodTestCase(TestCase):
 
-    @mock.patch('requests.request')
-    def _assert_method_is_called_with_expected_parameters(self, mocked_request_function,
-                                                          method_to_test, method_args,
-                                                          method_kwargs, expected_request_args,
-                                                          expected_request_kwargs):
-        method_to_test(*method_args, **method_kwargs)
-        mocked_request_function.assert_called_once()
-        mocked_request_function.assert_called_with(*expected_request_args,
-                                                   **expected_request_kwargs)
+        @mock.patch('requests.request')
+        def _assert_method_is_called_with_expected_parameters(self, mocked_request_function,
+                                                              method_to_test, method_args,
+                                                              method_kwargs, expected_request_args,
+                                                              expected_request_kwargs):
+            method_to_test(*method_args, **method_kwargs)
+            mocked_request_function.assert_called_once()
+            mocked_request_function.assert_called_with(*expected_request_args,
+                                                       **expected_request_kwargs)
 
-    @mock.patch('request.requests')
-    def _assert_method_formatter_passes(self, method_args, method_kwargs, expected_result,
-                                        mock_json, method, mock_resp):
-        """Assert that the given order returns the expected result in the expected format.
+        @mock.patch('requests.request')
+        def _assert_method_formatter_passes(self, method_args, method_kwargs, expected_result,
+                                            mock_json, method, mock_resp):
+            """Assert that the given order returns the expected result in the expected format.
 
-        :param method_args: Method arguments to pass as ``*args`` to the method.
-        :param method_kwargs:  Method keyword arguments to pass as ``**kwargs`` to the method.
-        :param expected_result: The expected Tuple
-        :param mock_json: The json data to feed the formatter
-        :param method: The method to call
-        :param mock_resp: a ``MockedResponse`` instance
-        :return: None
-        """
-        mock_resp.side_effect = [MockResponse({'BTC-USD'}, 200),  # mock supported_pairs
-                                 MockResponse(mock_json, 200)]
-        resp = method(*method_args, **method_kwargs)
+            :param method_args: Method arguments to pass as ``*args`` to the method.
+            :param method_kwargs:  Method keyword arguments to pass as ``**kwargs`` to the method.
+            :param expected_result: The expected Tuple
+            :param mock_json: The json data to feed the formatter
+            :param method: The method to call
+            :param mock_resp: a ``MockedResponse`` instance
+            :return: None
+            """
+            mock_resp.side_effect = [MockResponse({'BTC-USD'}, 200),  # mock supported_pairs
+                                     MockResponse(mock_json, 200)]
+            resp = method(*method_args, **method_kwargs)
 
-        self.assertIsInstance(resp, APIResponse)
-        self.assertEqual(method_args, expected_result, resp.formatted)
+            self.assertIsInstance(resp, APIResponse)
+            self.assertEqual(method_args, expected_result, resp.formatted)
 
-    def test_ticker_formatter(self, expected_result, mock_json, method_args=None,
-                              method_kwargs=None):
-        template_args = ['BTC-USD']
-        template_args += method_args or []
-        template_kwargs = {}
-        method_kwargs.update(method_kwargs or {})
-        self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
-                                             mock_json, self.exchange.ticker)
-
-    def test_order_book_formatter(self, expected_result, mock_json, method_args=None,
+        def test_ticker_formatter(self, expected_result, mock_json, method_args=None,
                                   method_kwargs=None):
-        template_args = ['BTC-USD']
-        template_args += method_args or []
-        template_kwargs = {}
-        method_kwargs.update(method_kwargs or {})
-        self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
-                                             mock_json, self.exchange.order_book)
+            template_args = ['BTC-USD']
+            template_args += method_args or []
+            template_kwargs = {}
+            template_kwargs.update(method_kwargs or {})
+            self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
+                                                 mock_json, self.exchange.ticker)
 
-    def test_trades_formatter(self, expected_result, mock_json, method_args=None,
-                              method_kwargs=None):
-        template_args = ['BTC-USD']
-        template_args += method_args or []
-        template_kwargs = {}
-        method_kwargs.update(method_kwargs or {})
-        self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
-                                             mock_json, self.exchange.trades)
+        def test_order_book_formatter(self, expected_result, mock_json, method_args=None,
+                                      method_kwargs=None):
+            template_args = ['BTC-USD']
+            template_args += method_args or []
+            template_kwargs = {}
+            template_kwargs.update(method_kwargs or {})
+            self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
+                                                 mock_json, self.exchange.order_book)
 
-    def test_ask_formatter(self, expected_result, mock_json, method_args=None, method_kwargs=None):
-        template_args = ['BTC-USD', 1000, 50]
-        template_args += method_args or []
-        template_kwargs = {}
-        method_kwargs.update(method_kwargs or {})
-        self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
-                                             mock_json, self.exchange.ask)
+        def test_trades_formatter(self, expected_result, mock_json, method_args=None,
+                                  method_kwargs=None):
+            template_args = ['BTC-USD']
+            template_args += method_args or []
+            template_kwargs = {}
+            template_kwargs.update(method_kwargs or {})
+            self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
+                                                 mock_json, self.exchange.trades)
 
-    def test_bid_formatter(self, expected_result, mock_json, method_args=None, method_kwargs=None):
-        template_args = ['BTC-USD', 1000, 50]
-        template_args += method_args or []
-        template_kwargs = {}
-        method_kwargs.update(method_kwargs or {})
-        self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
-                                             mock_json, self.exchange.bid)
+        def test_ask_formatter(self, expected_result, mock_json, method_args=None, method_kwargs=None):
+            template_args = ['BTC-USD', 1000, 50]
+            template_args += method_args or []
+            template_kwargs = {}
+            template_kwargs.update(method_kwargs or {})
+            self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
+                                                 mock_json, self.exchange.ask)
 
-    def test_order_status_formatter(self, expected_result, mock_json, method_args=None,
-                                    method_kwargs=None):
-        template_args = ['My_Order_ID']
-        template_args += method_args or []
-        template_kwargs = {}
-        method_kwargs.update(method_kwargs or {})
-        self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
-                                             mock_json, self.exchange.order_status)
+        def test_bid_formatter(self, expected_result, mock_json, method_args=None, method_kwargs=None):
+            template_args = ['BTC-USD', 1000, 50]
+            template_args += method_args or []
+            template_kwargs = {}
+            template_kwargs.update(method_kwargs or {})
+            self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
+                                                 mock_json, self.exchange.bid)
 
-    def test_open_orders_formatter(self, expected_result, mock_json, method_args=None,
-                                   method_kwargs=None):
-        template_args = []
-        template_args += method_args or []
-        template_kwargs = {}
-        method_kwargs.update(method_kwargs or {})
-        self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
-                                             mock_json, self.exchange.open_orders)
+        def test_order_status_formatter(self, expected_result, mock_json, method_args=None,
+                                        method_kwargs=None):
+            template_args = ['My_Order_ID']
+            template_args += method_args or []
+            template_kwargs = {}
+            template_kwargs.update(method_kwargs or {})
+            self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
+                                                 mock_json, self.exchange.order_status)
 
-    def test_cancel_order_formatter(self, expected_result, mock_json, method_args=None,
-                                    method_kwargs=None):
-        template_args = ['My_Order_ID']
-        template_args += method_args or []
-        template_kwargs = {}
-        method_kwargs.update(method_kwargs or {})
-        self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
-                                             mock_json, self.exchange.cancel_order)
+        def test_open_orders_formatter(self, expected_result, mock_json, method_args=None,
+                                       method_kwargs=None):
+            template_args = []
+            template_args += method_args or []
+            template_kwargs = {}
+            template_kwargs.update(method_kwargs or {})
+            self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
+                                                 mock_json, self.exchange.open_orders)
 
-    def test_wallet_formatter(self, expected_result, mock_json, method_args=None,
-                              method_kwargs=None):
-        template_args = ['BTC-USD']
-        template_args += method_args or []
-        template_kwargs = {}
-        method_kwargs.update(method_kwargs or {})
-        self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
-                                             mock_json, self.exchange.wallet)
+        def test_cancel_order_formatter(self, expected_result, mock_json, method_args=None,
+                                        method_kwargs=None):
+            template_args = ['My_Order_ID']
+            template_args += method_args or []
+            template_kwargs = {}
+            template_kwargs.update(method_kwargs or {})
+            self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
+                                                 mock_json, self.exchange.cancel_order)
+
+        def test_wallet_formatter(self, expected_result, mock_json, method_args=None,
+                                  method_kwargs=None):
+            template_args = ['BTC-USD']
+            template_args += method_args or []
+            template_kwargs = {}
+            template_kwargs.update(method_kwargs or {})
+            self._assert_method_formatter_passes(template_args, template_kwargs, expected_result,
+                                                 mock_json, self.exchange.wallet)
 
