8c8a310e9978ec15ace95a45f2eb47b24b17da98
nlsdfnbch <23okrs35+github@mykolab.com>
2018-01-18 13:41:18 +0100

    added remaining formatter dummies

---
+++ bitex/formatters/vaultoro.py
@@ -0,0 +1,20 @@
+# Import Built-ins
+from datetime import datetime
+
+# Import Home-brewed
+from bitex.formatters.base import APIResponse
+
+
+class VaultoroFormattedResponse(APIResponse):
+
+    def ticker(self, *args):
+        data = self.json(parse_int=str, parse_float=str)
+        bid = data["bidPrice"]
+        ask = data["askPrice"]
+        high = data["highPrice"]
+        low = data["lowPrice"]
+        last = data["lastPrice"]
+        volume = data["volume"]
+        timestamp = datetime.utcnow()
+        return super(VaultoroFormattedResponse, self).ticker(bid, ask, high, low, last, volume,
+                                                         timestamp)
