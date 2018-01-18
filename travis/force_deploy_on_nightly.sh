34582a4792f73057bd60d83940619a93192ca5fc
nlsdfnbch <23okrs35+github@mykolab.com>
2018-01-18 16:51:14 +0100

    fix equality testing

--- travis/force_deploy_on_nightly.sh
+++ travis/force_deploy_on_nightly.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $TRAVIS_BRANCH -eq nightly ]; then
+if [ "$TRAVIS_BRANCH" == "nightly" ]; then
     python generate_nightly_setup.py
     python setup.py sdist
     twine upload --username $PYPI_NAME --password $PYPI_PW dist/*
