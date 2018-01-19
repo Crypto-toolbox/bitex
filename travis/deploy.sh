e7406c200fb629e1356608e04a245ead2319177f
nlsdfnbch <23okrs35+github@mykolab.com>
2018-01-19 11:35:52 +0100

    drop use of force_deploy, update deploy.sh and travis yml

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,4 +1,18 @@
-if [[$TRAVIS_TAG == ^.*rc[0-9][0-9]*$ ]]
-    python generate_rc_setup.py $TRAVIS_TAG
+if [ "$TRAVIS_TAG" =~ ^.*rc[0-9][0-9]*$ ]; then
+    echo "ReleaseCandidate Tag found, running RC setup.."
+    python generate_rc_setup.py $TRAVIS_TAG && echo "done!"
+elif [ $TRAVIS_BRANCH" = "nightly" ]; then
+    echo "Branch is nightly, running nightly setup.."
+    python generate_nightly_setup.py && echo "done!"
 fi
-python setup.py sdist && twine upload --username $PYPI_NAME --password $PYPI_PW dist/*
+echo "Building source distribution.."
+python setup.py sdist
+
+if [ $? -eq 1 ]; then
+    echo "Build complete. Initializing upload via twine.."
+    twine upload --username $PYPI_NAME --password $PYPI_PW dist/* && echo "Upload successful!" || echo "Upload failed!"
+else
+    echo "Buildind source distribution failed. Skipping upload."
+fi
+
+echo "Deploy script complete."
