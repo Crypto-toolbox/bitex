93e3af2ce6a8e2db743e430f69217da67e5a89bb
nils.diefenbach <nils.diefenbach@celeraone.com>
2018-12-09 15:02:48 +0100

    Update setup files for v3

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.2.12'
+VERSION = '3.0.0'
 
 
-setup(name='BitEx', version=VERSION, author='Nils Diefenbach',
-      author_email='23okrs20+pypi@mykolab.com',
+setup(
+      name='BitEx',
+      version=VERSION,
+      author='Nils Diefenbach',
+      author_email='nlsdfnbch.foss@kolabnow.com',
       url="https://github.com/nlsdfnbch/bitex.git",
-      test_suite='nose.collector', tests_require=['nose'],
-      packages=find_packages(exclude=['contrib', 'docs', 'tests*', 'travis']),
-      install_requires=['requests', 'websocket-client', 'autobahn', 'pusherclient'],
+      packages=find_packages(exclude=['docs', 'tests']),
+      install_requires=['requests', 'pusherclient'],
       description='Python3-based API Framework for Crypto Exchanges',
-      license='MIT',  classifiers=['Development Status :: 4 - Beta',
-                                   'Intended Audience :: Developers'],
+      license='MIT',
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers'
+      ],
 )
