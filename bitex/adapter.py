9848507fb540f092aa56e6ff10e79112b82778bb
nils.diefenbach <nils.diefenbach@celeraone.com>
2019-02-15 10:43:02 +0100

    issue-#202 Inject BitexResponse into build_response().
    Extended requests.HTTPAdapter to enable us to inject a custom
    response class. This defaults to bitex.BitexResponse, which
    offers no additional features to the usual requests.Response
    class.

---
+++ bitex/adapter.py
@@ -0,0 +1,65 @@
+from urllib3.response import HTTPResponse
+
+from requests.adapters import HTTPAdapter
+from requests.cookies import extract_cookies_to_jar
+from requests.structures import CaseInsensitiveDict
+from requests.utils import get_encoding_from_headers
+
+from bitex.plugins import PLUGINS
+from bitex.request import BitexPreparedRequest
+from bitex.response import BitexResponse
+
+
+class BitexHTTPAdapter(HTTPAdapter):
+    """Custom HTTP Adapter for :mod:`Bitex`.
+
+    It replaces :cls:`requests.Respopnse` as the default response class when
+    building the response, with either an adequate plugin-supplied class or
+    :mod:`bitex` 's own default :cls:`BitexResponse` class.
+    """
+
+    def build_response(self, req: BitexPreparedRequest, resp: HTTPResponse) -> BitexResponse:
+        """Build a :cls:`BitexResponse` from the given `req` and `resp`,
+
+        The method is largely identical to :meth:`HTTPAdapter.build_response`,
+        and only differs in the class type used when constructing a response.
+
+        This class is taken firstly from any valid plugin that supplies an
+        adequate class for the exchange that was queried (as stated in
+        :attr:`BitexPreparedRequest.exchange`), or :mod:`bitex` 's own default
+        :cls:`BitexResponse` class.
+
+        :param BitexPreparedRequest req:
+            The :cls:`BitexPreparedRequest` used to generate the response.
+        :param HTTPResponse resp: The urllib3 response object.
+        :rtype: BitexResponse
+        """
+        if req.exchange in PLUGINS:
+            response = PLUGINS[req.exchange]['Response']()
+        else:
+            response = BitexResponse()
+
+        # Fallback to None if there's no status_code, for whatever reason.
+        response.status_code = getattr(resp, 'status', None)
+
+        # Make headers case-insensitive.
+        response.headers = CaseInsensitiveDict(getattr(resp, 'headers', {}))
+
+        # Set encoding.
+        response.encoding = get_encoding_from_headers(response.headers)
+        response.raw = resp
+        response.reason = response.raw.reason
+
+        if isinstance(req.url, bytes):
+            response.url = req.url.decode('utf-8')
+        else:
+            response.url = req.url
+
+        # Add new cookies from the server.
+        extract_cookies_to_jar(response.cookies, req, resp)
+
+        # Give the Response some context.
+        response.request = req
+        response.connection = self
+
+        return response
---
+++ tests/test_adapter.py
@@ -0,0 +1,46 @@
+import pytest
+
+from unittest.mock import patch
+
+from bitex.adapter import BitexHTTPAdapter, BitexResponse, BitexPreparedRequest, CaseInsensitiveDict
+
+
+@pytest.fixture
+def mock_url_response():
+    """Simplistic urllib3.response.HTTPResponse Mock.
+
+    Provides minimal set of attributes and values for these to construct a
+    BitexResponse using :meth:`BitexHTTPAdaper.build_response`.
+    """
+    class MockResponse:
+        def __init__(self):
+            self.status = 200
+            self.reason = "OK"
+            self.headers = {"ACCESS": "Granted!"}
+
+    return MockResponse()
+
+
+@patch.dict("bitex.adapter.PLUGINS", {})
+@patch("bitex.adapter.get_encoding_from_headers", return_value="UTF-512")
+@patch("bitex.adapter.extract_cookies_to_jar")
+def test_adapter_returns_bitex_response_by_default(mock_extract_cookies, mock_get_encoding, mock_url_response):
+    request = BitexPreparedRequest("TestExchange")
+    request.url = "http://bitex.com"
+
+    adapter = BitexHTTPAdapter()
+    bitex_response = adapter.build_response(request, mock_url_response)
+    assert isinstance(bitex_response, BitexResponse)
+
+    # Ensure method otherwise operates identically to the parent class's method.
+    assert mock_extract_cookies.called
+    assert mock_get_encoding.called
+    assert bitex_response.encoding == "UTF-512"
+    assert bitex_response.url == "http://bitex.com"
+    assert bitex_response.status_code == 200
+    assert bitex_response.reason == "OK"
+    assert bitex_response.headers == CaseInsensitiveDict({"ACCESS": "Granted!"})
+    assert bitex_response.raw == mock_url_response
+    assert bitex_response.request == request
+    assert bitex_response.connection == adapter
+
