1e9b29d47e079027230135f0bf167cf9e6728990
nils.diefenbach <nils.diefenbach@celeraone.com>
2019-02-15 13:55:44 +0100

    issue-#207 Inject custom classes when preparing request.
    This commit also adds custom implementations of
    requests.Request and requests.PreparedRequest, which
    replace the latter two automatically.
    
    Classes supplied by plugins take precedence.

---
+++ bitex/request.py
@@ -0,0 +1,91 @@
+import urllib3
+
+from requests import Request, PreparedRequest
+from urllib3.exceptions import LocationParseError
+from requests.packages.urllib3.util import parse_url
+
+from bitex.plugins import PLUGINS, HOOKS
+from bitex.constants import BITEX_SHORTHAND_NO_ACTION_REGEX, BITEX_SHORTHAND_WITH_ACTION_REGEX
+
+
+class BitexPreparedRequest(PreparedRequest):
+    def __init__(self, exchange):
+        self.exchange = exchange
+        super(BitexPreparedRequest, self).__init__()
+
+    @staticmethod
+    def check_url_for_shorthand(url):
+        """Check if the given URL is a bitex short-hand.
+
+        If it is, we return the value of :meth:`re.Match.groupdict`; otherwise
+        we return None instead.
+        """
+        con_action = BITEX_SHORTHAND_WITH_ACTION_REGEX.match(url)
+        sans_action = BITEX_SHORTHAND_NO_ACTION_REGEX.match(url)
+        match = con_action or sans_action
+        try:
+            return match.groupdict()
+        except AttributeError:
+            # Not a valid shorthand url, return None
+            return None
+
+
+class BitexRequest(Request):
+
+    def __init__(self, private=False, **kwargs):
+        super(BitexRequest, self).__init__(**kwargs)
+        self.exchange = self.parse_target_exchange()
+        self.private = private
+
+    def __repr__(self):
+        return f'<BitexRequest [{self.method}]>'
+
+    def parse_target_exchange(self)-> str:
+        """Check the URL for its scheme and extract an exchange name, if any.
+
+        If the url starts with http/https we set :attr:`BitexRequest.exchange`
+        to `None`. Otherwise we store the `exchange` in said attribute.
+
+        ..Note::
+
+            We do not check whether or not this is an actual exchange name.
+            Schemes such as `mailto:` are not checked for and may cause errors.
+            Hence, only HTTP urls or :mod:`bitex` short-hand urls are supported
+            by :mod:`bitex`!
+        """
+        try:
+            parse_url(self.url).scheme
+        except LocationParseError:
+            # This string isn't parsable by urllib - it may be a shorthand.
+            scheme, _ = self.url.split(":", maxsplit=1)
+            if scheme:
+                return scheme
+        else:
+            return None
+
+    def prepare(self) -> BitexPreparedRequest:
+        """Constructs a :class:`BitexPreparedRequest` for transmission and returns it.
+
+        .. Note::
+
+            Unlike :meth:BitexSession.prepare_request, this method does *not*
+            apply a custom auth class automatically, if no auth object was given.
+        """
+        custom_classes = PLUGINS.get(self.exchange)
+        if custom_classes:
+            p = custom_classes['PreparedRequest'](self.exchange)
+        else:
+            p = BitexPreparedRequest(self.exchange)
+        p.prepare(
+            method=self.method,
+            url=self.url,
+            headers=self.headers,
+            files=self.files,
+            data=self.data,
+            json=self.json,
+            params=self.params,
+            auth=self.auth,
+            cookies=self.cookies,
+            hooks=self.hooks,
+        )
+        return p
---
+++ tests/test_request.py
@@ -0,0 +1,68 @@
+from unittest.mock import patch, MagicMock
+
+import pytest
+
+from bitex.request import BitexRequest, BitexPreparedRequest
+
+
+class TestBitexRequest:
+
+    @pytest.mark.parametrize(
+        argnames="url, expected_target",
+        argvalues=[
+            ("https://google.com", None),
+            ("http://google.com", None),
+            ("google:something.com/else", "google")
+        ]
+    )
+    def test_parse_target_exchange_parses_schemes_and_returns_correctly(
+            self, url, expected_target):
+        req = BitexRequest(url=url)
+        # Assert the target exchange is parsed on __init__()
+        assert req.exchange == expected_target
+
+        # Assert the method itself returns correctly and the previously checked
+        # attribute was not a default value.
+        assert req.parse_target_exchange() == expected_target
+
+    @patch("bitex.request.BitexPreparedRequest.prepare")
+    def test_prepare_uses_custom_class_if_available(self, mock_prep_prepare):
+        request = BitexRequest(url="test:instrument/ticker")
+        custom_class = MagicMock(name="PseudoCustomClass")
+        custom_class.return_value = custom_class
+        assert request.exchange == "test"
+        with patch.dict("bitex.request.PLUGINS", {"test": {"PreparedRequest": custom_class}}):
+            assert request.prepare() == custom_class
+        assert not mock_prep_prepare.called
+        assert custom_class.called
+        custom_class.assert_called_once_with("test")
+
+    @patch("bitex.request.BitexPreparedRequest.prepare")
+    def test_prepare_defaults_to_bitex_prepared_request_if_no_custom_class_is_available(self, _):
+        request = BitexRequest(url="test:instrument/endpoint")
+        assert isinstance(request.prepare(), BitexPreparedRequest)
+
+
+class TestBitexPreparedRequest:
+    def test_check_url_for_shorthand_successfully_parses_shorthands_without_an_action(self):
+        url = "MyExchange:MyPair/ticker"
+        result = BitexPreparedRequest.check_url_for_shorthand(url)
+        assert result is not None
+        assert result["exchange"] == "MyExchange"
+        assert result["instrument"] == "MyPair"
+        assert result["endpoint"] == "ticker"
+
+    def test_check_url_for_shorthand_successfully_parses_shorthands_with_an_action(self):
+        url = "MyExchange:MyPair/order/new"
+        result = BitexPreparedRequest.check_url_for_shorthand(url)
+        assert result is not None
+        assert result["exchange"] == "MyExchange"
+        assert result["instrument"] == "MyPair"
+        assert result["endpoint"] == "order"
+        assert result["action"] == "new"
+
+    def test_check_url_for_shorthand_returns_none_if_the_shorthand_is_not_valid(self):
+        url = "https://somehwere.com/123"
+        result = BitexPreparedRequest.check_url_for_shorthand(url)
+        assert result is None
+
