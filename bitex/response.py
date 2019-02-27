0ef53ff6156c955c4ec2daa4c09c6b5c3dfaf3c1
nils.diefenbach <nils.diefenbach@celeraone.com>
2019-02-27 13:19:58 +0100

    no-issue Add return types to response methods.

--- bitex/response.py
+++ bitex/response.py
@@ -1,3 +1,4 @@
+from typing import Union, Dict
 from requests.models import Response
 
 
@@ -7,7 +8,7 @@ class BitexResponse(Response):
     def __repr__(self):
         return '<BitexResponse [%s]>' % (self.status_code)
 
-    def triples(self):
+    def triples(self) -> List[Tuple[int, str, Union[str, int, float]], ...]:
         """Return the data of the response in three-column layout.
 
         Data is returned as a list of 3-item tuples::
@@ -20,7 +21,7 @@ class BitexResponse(Response):
         """
         raise NotImplementedError
 
-    def key_value_dict(self):
+    def key_value_dict(self) -> Dict[str, Union[str, int, float]]:
         """Return the data of the response in a flattened dict.
 
         This provides the data as a dict of key-value pairs, which is ready for
