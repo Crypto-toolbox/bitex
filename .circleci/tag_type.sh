cff325d66a831ff885c264811f90d7af22e173a6
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-11-22 12:25:06 +0100

    Update CI config

--- .circleci/tag_type.sh
+++ .circleci/tag_type.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
 COMMIT=$(git log --format=oneline -n 1 HEAD --format=%s)
-echo ${COMMIT:0:4}
+if [[ "${COMMIT:0:4}" == "FEAT" ]]; then
+    echo minor
+elif [[ "${COMMIT:0:4}" == "FIX"* ]]; then
+    echo patch
+else
+    echo
+fi
